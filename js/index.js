2a137e209128bd765a11d9224644248c2b9dfbec
Владислав <arshavin0309@mail.ru>
2024-08-21 15:26:59 +0500

    add

--- js/index.js
+++ js/index.js
@@ -15,12 +15,33 @@ document.addEventListener('DOMContentLoaded', () => {
         spaceBetween: 30,
         centeredSlides: true,
         autoplay: {
-          delay: 3500,
-          disableOnInteraction: false,
+            delay: 3500,
+            disableOnInteraction: false,
         },
         pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
+            el: ".swiper-pagination",
+            clickable: true,
         },
-      });
+    });
+
+    let scrollPos = 0;
+    let joinUsImg = document.querySelector('.join-us__img');
+    let howContainer = document.querySelector('.how .container');
+    let doorsDecor = document.querySelector('.doors__decor');
+
+    $(window).scroll(function () {
+        var st = $(this).scrollTop();
+
+        if (st > scrollPos) {
+            joinUsImg.classList.remove('reverse');
+            howContainer.classList.remove('reverse');
+            doorsDecor.classList.remove('reverse');
+        } else {
+            joinUsImg.classList.add('reverse');
+            howContainer.classList.add('reverse');
+            doorsDecor.classList.add('reverse');
+        };
+
+        scrollPos = st;
+    });
 });
