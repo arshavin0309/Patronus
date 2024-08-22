453bfa55c0bf727107bdb63e95b8c6dee78a1334
Владислав <arshavin0309@mail.ru>
2024-08-22 13:52:26 +0500

    add

--- app/js/main.js
+++ app/js/main.js
@@ -1,48 +1,49 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const swiper1 = new Swiper('.swiper1', {
-        loop: true,
+const swiper1 = new Swiper('.swiper1', {
+    loop: true,
 
-        slidesPerView: 3,
-        spaceBetween: 20,
-        // mousewheel: true,
-        mousewheel: { eventsTarget: 'body' },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-    });
+    slidesPerView: 3,
+    spaceBetween: 20,
+    // mousewheel: true,
+    mousewheel: { eventsTarget: 'body' },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
 
-    var swiper2 = new Swiper(".swiper2", {
-        spaceBetween: 30,
-        centeredSlides: true,
-        autoplay: {
-            delay: 3500,
-            disableOnInteraction: false,
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-    });
+var swiper2 = new Swiper(".swiper2", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
 
-    let scrollPos = 0;
-    let joinUsImg = document.querySelector('.join-us__img');
-    let howContainer = document.querySelector('.how .container');
-    let doorsDecor = document.querySelector('.doors__decor');
+let scrollPos = 0;
+let joinUsImg = document.querySelector('.join-us__img');
+let howContainer = document.querySelector('.how .container');
+let doorsDecor = document.querySelector('.doors__decor');
+let acquaintanceImage = document.querySelector('.acquaintance__image');
 
-    $(window).scroll(function () {
-        var st = $(this).scrollTop();
+$(window).scroll(function () {
+    var st = $(this).scrollTop();
 
-        if (st > scrollPos) {
-            joinUsImg.classList.remove('reverse');
-            howContainer.classList.remove('reverse');
-            doorsDecor.classList.remove('reverse');
-        } else {
-            joinUsImg.classList.add('reverse');
-            howContainer.classList.add('reverse');
-            doorsDecor.classList.add('reverse');
-        };
+    if (st > scrollPos) {
+        if (joinUsImg) { joinUsImg.classList.remove('reverse'); };
+        if (howContainer) { howContainer.classList.remove('reverse'); };
+        if (doorsDecor) { doorsDecor.classList.remove('reverse'); };
+        if (acquaintanceImage) { acquaintanceImage.classList.remove('reverse'); };
+    } else {
+        if (joinUsImg) { joinUsImg.classList.add('reverse'); };
+        if (howContainer) { howContainer.classList.add('reverse'); };
+        if (doorsDecor) { doorsDecor.classList.add('reverse'); };
+        if (acquaintanceImage) { acquaintanceImage.classList.add('reverse'); };
+    };
 
-        scrollPos = st;
-    });
+    scrollPos = st;
 });
